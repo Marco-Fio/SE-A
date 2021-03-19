429d6904f240ac379faa0185cf6f4cb0d28403ef
Janik Noldin <teeracer90@gmail.com>
2021-03-19 15:49:47 +0100

    finished ar placement

---
+++ Assets/Scripts/GameScripts/SelectionController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionController : MonoBehaviour
+{
+
+    public List<PlacementObject> placedObjects;
+    public Camera arCamera;
+    private ObjectSpawnerScript objectSpawnerScript; 
+    public Text textObject;
+    private GameObject lastSelected;
+    private Vector3 lastPos;
+
+    private void Start()
+    {
+        objectSpawnerScript = FindObjectOfType<ObjectSpawnerScript>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!objectSpawnerScript.objPlaced)
+        {
+            return;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    GameObject placementObject = hitObject.collider.gameObject;
+                    ChangeSelectedObject(placementObject);
+                } else
+                {
+                    lastSelected = null;
+                }
+            }
+        }
+    }
+
+    private void ChangeSelectedObject(GameObject selected)
+    {
+        if(lastSelected != null)
+        {
+            lastSelected.transform.position = lastPos;
+        }
+        lastPos = selected.transform.position;
+
+        textObject.text = lastPos.ToString();
+        lastSelected = selected;
+    }
+}
---
+++ Assets/Scripts/GameScripts/UIControlScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControlScript : MonoBehaviour
+{
+    private GameObject field;
+    public int rotateAngle;
+    public int ScaleFactor;
+    private Vector3 scaleFactor;
+    private ObjectSpawnerScript objectSpawner;
+
+    void Awake()
+    {
+        scaleFactor = new Vector3(ScaleFactor, ScaleFactor, ScaleFactor);
+        field = GameObject.Find("Phase1");
+        objectSpawner = FindObjectOfType<ObjectSpawnerScript>();
+    }
+
+    public void ResetBtnClick()
+    {
+        Destroy(field);
+        GameObject.Find("Panel").SetActive(false);
+        GameObject.Find("PlacementIndicator").SetActive(true);
+        objectSpawner.objPlaced = false;
+    }
+
+    public void RotateLeftBtnClick()
+    {
+        field.transform.Rotate(Vector3.up, rotateAngle);
+    }
+
+    public void RotateRightBtnClick()
+    {
+        field.transform.Rotate(Vector3.up, -rotateAngle);
+    }
+
+    public void ScaleUpBtnClick()
+    {
+        field.transform.localScale += scaleFactor;
+    }
+
+    public void ScaleDownBtnClick()
+    {
+        field.transform.localScale -= scaleFactor;
+    }
+
+    public void AcceptBtnClick()
+    {
+        
+    }
+}
